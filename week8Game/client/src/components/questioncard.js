99c0393c3784db31708a4d43ba399e47f2f902d5
Roshanell Francisco <96213223+Roshanell@users.noreply.github.com>
2023-03-08 14:18:28 -0600

    randmizing multiple choice answers

--- week8Game/client/src/components/questioncard.js
+++ week8Game/client/src/components/questioncard.js
@@ -1,12 +1,36 @@
+import { useEffect, useState } from "react";
+
 const QuestionCard = (props) => {
+	const [randomAnswers, setRandomAnswers] = useState([]);
+	useEffect(() => {
+		if (randomAnswers.length) return;
+		const newRandomAnswers = [];
+
+		let answers = [0, 1, 2, 3];
+		while (answers.length > 0) {
+			const answer = answers.splice(
+				Math.floor(Math.random() * answers.length),
+				1
+			)[0];
+			newRandomAnswers.push(answer);
+		}
+		setRandomAnswers(newRandomAnswers);
+	}, []);
+
 	return (
 		<div className={"question-section"}>
 			<div className="question-text">{props.question.question}</div>
 			<div className="answer-section">
-				<button>{props.question.correct_answer}</button>
-				<button>{props.question.incorrect_answers[0]}</button>
-				<button>{props.question.incorrect_answers[1]}</button>
-				<button>{props.question.incorrect_answers[2]}</button>
+				{randomAnswers.map((answer) => {
+					if (answer === 3) {
+						return (
+							<button className="correct">
+								{props.question.correct_answer}
+							</button>
+						);
+					}
+					return <button>{props.question.incorrect_answers[answer]}</button>;
+				})}
 			</div>
 		</div>
 	);
